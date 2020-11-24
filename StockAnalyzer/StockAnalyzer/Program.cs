270a5a11aca61aaaae7794e3e8d1bbfc0764e5af
Monika <m.szymanska.macik@gmail.com>
2020-11-24 22:10:29 +0100

    time in Sydney checked

--- StockAnalyzer/StockAnalyzer/Program.cs
+++ StockAnalyzer/StockAnalyzer/Program.cs
@@ -13,9 +13,16 @@ namespace StockAnalyzer
             foreach(var line in lines.Skip(1))
             {
                 var segments = line.Split(",");
-                var tradeDate = DateTime.Parse(segments[1]);
+                var tradeDate = DateTime.Parse(segments[1], CultureInfo.GetCultureInfo("en-GB"));
                 Console.WriteLine(tradeDate.ToLongDateString());
             }
+
+            var now = DateTime.Now;
+            TimeZoneInfo sydneyTimeZone = TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
+            var sydneyTime = TimeZoneInfo.ConvertTime(now, sydneyTimeZone);
+
+            Console.WriteLine(now);
+            Console.WriteLine(sydneyTime);
         }
     }
 }
